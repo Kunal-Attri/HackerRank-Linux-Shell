2c3b32f9bb633c6de6f793348be0cb6b7bd0c6cf
Kunal Attri <attrikunal16@gmail.com>
2022-05-29 13:46:39 +0530

    8th

---
+++ More on Conditionals.sh
@@ -0,0 +1,17 @@
+read x
+read y
+read z
+
+if [ $x -eq $y ]; then
+    if [ $y -eq $z ]; then
+        echo "EQUILATERAL"
+    else
+        echo "ISOSCELES"
+    fi
+elif [ $y -eq $z ]; then
+    echo "ISOSCELES"
+elif [ $x -eq $z ]; then
+    echo "ISOSCELES"
+else
+    echo "SCALENE"
+fi
